7d528aa0cfec612f309e272fc68aa90755414dd3
Soumik Dutta <shalearkane@gmail.com>
2024-11-06 22:58:18 +0530

    initial code for applying criteria to get class data

---
+++ Soumik/criterion/illumination.py
@@ -0,0 +1,69 @@
+from skyfield.api import load, PlanetaryConstants
+from datetime import datetime, timezone
+
+
+ts = load.timescale()
+
+# Load ephemeris data for the Sun and the Moon
+eph = load("de421.bsp")
+moon = eph["moon"]
+sun = eph["Sun"]
+
+pc = PlanetaryConstants()
+pc.read_text(load("moon_080317.tf"))
+pc.read_text(load("pck00008.tpc"))
+pc.read_binary(load("moon_pa_de421_1900-2050.bpc"))
+
+frame = pc.build_frame_named("MOON_ME_DE421")
+
+
+def angular_difference_long(long1: float, long2: float) -> float:
+    # Ensure longitudes are in the range [-180, 180]
+    long1 = ((long1 + 180.0) % 360.0) - 180.0
+    long2 = ((long2 + 180.0) % 360.0) - 180.0
+
+    # Calculate the raw difference
+    diff = abs(long1 - long2)
+
+    # Adjust to get the smallest angular difference
+    if diff > 180.0:
+        diff = 360.0 - diff
+
+    return diff
+
+
+def angular_difference_lat(lat1: float, lat2: float) -> float:
+    # Ensure latitudes are in the range [-90, 90]
+    lat1 = max(min(lat1, 90.0), -90.0)
+    lat2 = max(min(lat2, 90.0), -90.0)
+
+    # Calculate the absolute difference
+    diff = abs(lat1 - lat2)
+
+    return diff
+
+
+def get_subsolar_latitude_longitude(dt: datetime) -> tuple[float, float]:
+    t = ts.utc(dt)
+    p = moon.at(t).observe(sun).apparent()
+    lat, long, distance = p.frame_latlon(frame)
+    long_degrees = (long.degrees + 180.0) % 360.0 - 180.0
+
+    return lat.degrees, long_degrees
+
+
+def check_if_illuminated(lat: float, long: float, dt: datetime) -> bool:
+    moon_lat, moon_long = get_subsolar_latitude_longitude(dt)
+    print(moon_lat, moon_long)
+
+    if angular_difference_lat(moon_lat, lat) > 44.0:
+        return False
+
+    if angular_difference_long(moon_long, long) > 89.0:
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+    print(check_if_illuminated(1.0, 30.0, datetime.now(timezone.utc)))
---
+++ Soumik/main.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+import astropy.io.fits as aif
+from astropy.table import Table
+from constants import *
+
+
+@dataclass
+class CLASS_HEADER:
+    latitude_0: float
+    latitude_1: float
+    latitude_2: float
+    latitude_3: float
+
+    longitude_0: float
+    longitude_1: float
+    longitude_2: float
+    longitude_3: float
+
+
+with aif.open("ch2_cla_l1_20210827T210316000_20210827T210332000_1024.fits") as hdul:
+    t = Table.read(hdul["SPECTRUM"])
+
+    for key, value in t.meta.items():
+        print(f"{key} = {value}")
+
+    ch = CLASS_HEADER(
+        float(t.meta[V0_LAT]),
+        float(t.meta[V1_LAT]),
+        float(t.meta[V2_LAT]),
+        float(t.meta[V3_LAT]),
+        float(t.meta[V0_LON]),
+        float(t.meta[V1_LON]),
+        float(t.meta[V2_LON]),
+        float(t.meta[V3_LON]),
+    )
+
+if
