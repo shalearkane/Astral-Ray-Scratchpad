0bcd2cf18c40d1b259e70cea6958e06996f55e46
Soumik Dutta <shalearkane@gmail.com>
2024-11-13 03:00:10 +0530

    commit mongodb constants

---
+++ Pratham/constants/mongo.py
@@ -0,0 +1,11 @@
+from typing import Final
+
+# MongoDB connection details
+MONGO_URI: Final[str] = "mongodb://localhost:27017"
+DATABASE_NAME: Final[str] = "ISRO"
+COLLECTION_CLASS_FITS: Final[str] = "primary"
+COLLECTION_CLASS_FITS_ACCEPTED: Final[str] = "class_fits_accepted"
+COLLECTION_CLASS_XSM_PRIMARY: Final[str] = "xsm_primary"
+
+KEY_PASSED_CHECK: Final[str] = "passed_check"
+KEY_IS_IN_GEOTAIL: Final[str] = "is_in_geotail"
