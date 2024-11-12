782f28cd41bae9826b7f7a04e7dbd04d77d27044
Soumik Dutta <shalearkane@gmail.com>
2024-11-13 05:28:01 +0530

    add function to fetch class files withing a time range

--- Pratham/CLASS/class.py
+++ Pratham/CLASS/class.py
@@ -1,4 +1,27 @@
-from datetime import datetime
+import sys
+sys.path.append(r'..')
 
-def get_class_file(start_time: datetime, end_time: datetime):
-    pass
+from datetime import datetime
+from pymongo import MongoClient
+from Pratham.constants.mongo import *
+
+
+def get_class_within_time_range(start_time: datetime, end_time: datetime) -> list[dict]:
+    class_fits_accepted = MongoClient(MONGO_URI)[DATABASE_NAME][
+        COLLECTION_CLASS_FITS_ACCEPTED
+    ]
+
+    cursor = class_fits_accepted.find(
+        filter={
+            "parsedStartTime": {"$gte": start_time},
+            "parsedEndTime": {"$lte": end_time},
+        }
+    )
+
+    return [doc for doc in cursor]
+
+
+if __name__ == "__main__":
+    start_time = datetime(2022, 10, 8, 10, 20, 15)
+    end_time = datetime(2022, 11, 9, 10, 40, 15)
+    print(get_class_within_time_range(start_time, end_time))
--- Pratham/GOES/goes.py
+++ Pratham/GOES/goes.py
@@ -1,4 +1,13 @@
 from datetime import datetime
+from typing import Tuple
 
-def add_buffer_time(start_time: datetime, end_time: datetime):
-    pass
+
+def add_buffer_time(
+    start_time: datetime, end_time: datetime
+) -> Tuple[datetime, datetime]:
+    time_diff = end_time - start_time
+
+    new_start_time = start_time - time_diff
+    new_end_time = end_time + time_diff
+
+    return new_start_time, new_end_time
