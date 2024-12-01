003b66d4f148443e13f3072188c148c18de72ae6
Soumik Dutta <shalearkane@gmail.com>
2024-12-01 07:55:58 +0530

    final parameters for combined-fits finding

--- Soumik/process-combine-fits.py
+++ Soumik/process-combine-fits.py
@@ -4,12 +4,12 @@ from helpers.download import download_file_from_file_server
 from helpers.combine_fits import combine_fits
 from helpers.query import get_class_fits_at_lat_lon
 
-increment: Final[float] = 0.2
+increment: Final[float] = 0.1
 
-latitude_start: Final[float] = 35
-latitude_end: Final[float] = 40
+latitude_start: Final[float] = 30
+latitude_end: Final[float] = 50
 
-longitude_start: Final[float] = 85
+longitude_start: Final[float] = 80
 longitude_end: Final[float] = 100
 
 
@@ -28,7 +28,7 @@ while latitude < latitude_end:
             if download_file_from_file_server(doc, "primary", download_folder):
                 file_paths.append(f"{download_folder}/{doc["path"].split("/")[-1]}")
 
-        combined_fits_path = f"data-generated/combined-fits/{latitude:.2f}_{longitude:.2f}.fits"
+        combined_fits_path = f"data-generated/combined-fits/{latitude:.1f}_{longitude:.1f}.fits"
         metadata = {"lat": latitude, "lon": longitude}
         if combine_fits(file_paths, combined_fits_path, metadata):
             print(f"generated: {combined_fits_path}")
