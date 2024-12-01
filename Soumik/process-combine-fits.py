f8122a99fdf69676a3a4b0003b1c5491b810e160
Soumik Dutta <shalearkane@gmail.com>
2024-12-01 08:10:17 +0530

    tweak code finally for easy resume

--- Soumik/process-combine-fits.py
+++ Soumik/process-combine-fits.py
@@ -5,6 +5,7 @@ import concurrent.futures
 from helpers.download import download_file_from_file_server
 from helpers.combine_fits import combine_fits
 from helpers.query import get_class_fits_at_lat_lon
+from os.path import isfile
 
 increment: Final[float] = 0.1
 
@@ -17,25 +18,32 @@ longitude_end: Final[float] = 100
 download_folder = "data/class"
 
 
-def generate_combined_fits_for_lat_lon(latitude: float, longitude: float):
+def generate_combined_fits_for_lat_lon(latitude: float, longitude: float, redo: bool) -> bool:
+
+    combined_fits_path = f"data-generated/combined-fits/{latitude:.1f}_{longitude:.1f}.fits"
+    if not redo and isfile(combined_fits_path):
+        print(f"already generated: {combined_fits_path}")
+        return True
+
     docs = get_class_fits_at_lat_lon(latitude, longitude)
     file_paths: List[str] = list()
     for doc in docs:
         if download_file_from_file_server(doc, "primary", download_folder):
             file_paths.append(f"{download_folder}/{doc["path"].split("/")[-1]}")
 
-    combined_fits_path = f"data-generated/combined-fits/{latitude:.1f}_{longitude:.1f}.fits"
     metadata = {"lat": latitude, "lon": longitude}
     if combine_fits(file_paths, combined_fits_path, metadata):
         print(f"generated: {combined_fits_path}")
+        return True
     else:
         print(f"could not generate: {combined_fits_path}")
+        return False
 
 
 def do_in_parallel(lat_lon_pairs: List[Tuple[float, float]]):
     results = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_latlon = {executor.submit(generate_combined_fits_for_lat_lon, lat, lon): (lat, lon) for lat, lon in lat_lon_pairs}
+        future_to_latlon = {executor.submit(generate_combined_fits_for_lat_lon, lat, lon, False): (lat, lon) for lat, lon in lat_lon_pairs}
         for future in concurrent.futures.as_completed(future_to_latlon):
             try:
                 results.append(future.result())
