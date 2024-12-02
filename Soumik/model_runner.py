a17a35abc6d1dbf2d41a2e50dc0e648371dd1c3a
Arnaav Shah <arnaaavshaah@gmail.com>
2024-12-02 14:43:43 +0530

    fix client.py

--- Soumik/model_runner.py
+++ Soumik/model_runner.py
@@ -1,9 +1,9 @@
 from model.model_generic import process_abundance
 
 
-class_l1 = "/home/sm/Public/Inter-IIT/Astral-Ray-Scratchpad/Soumik/data-generated/combined-fits/35.2_85.2.fits"
+class_l1 = "/tmp/worker_2_34.0_96.6.fits.fits"
 background = "model/data/reference/background_allevents.fits"
-solar = "/home/sm/Public/Inter-IIT/Astral-Ray-Scratchpad/Soumik/data/flux/some.txt"
-scatter_atable = "/home/sm/Public/Inter-IIT/Astral-Ray-Scratchpad/Soumik/scatter/some.fits"
+solar = "data-generated/client/some.txt"
+scatter_atable = "data-generated/client/some.fits"
 
 abundance = process_abundance(class_l1, background, solar, scatter_atable, bin_size=2048)
